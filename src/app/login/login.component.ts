ac786e0b709e18776ce9358e8ed491dba2324a90
Kevin Buckley <kevin.buckley2013@gmail.com>
2020-08-12 14:32:16 -0400

    Adds hardcoded validation, improper way to use validation but samples ngIf

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  username = '';
+  password = '';
+  data = {};
+  errorMessage = 'Invalid Credentials';
+  invalidLogin = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleLogin() {
+    if (this.username === 'sample' && this.password === 'bruh') {
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
+    this.data = { username: this.username, password: this.password };
+    console.log(this.username, this.password);
+    console.log(this.data);
+    return this.data;
   }
-
 }
